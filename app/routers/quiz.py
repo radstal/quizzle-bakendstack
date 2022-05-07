7c7de788ab7c96fba2aee197a8cd9d348f141b45
suchat tangjarukij <suchat.tangjarukij@gmail.com>
2022-05-07 13:07:52 +1000

    fix quiz/{id} dtype

--- app/routers/quiz.py
+++ app/routers/quiz.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 from connections import QUIZ_COL
-from schema.schema import QUIZ
+from schema.schema import QUIZ,QUIZES
 import json 
+from bson import ObjectId
 
 router = APIRouter()
 
@@ -10,12 +11,13 @@ router = APIRouter()
 async def createQuiz(quiz: QUIZ):
     return {"message": "created"}
 
-@router.get("/") #READBULK
+@router.get("/",response_model=QUIZES) #READBULK
 async def listQuiz():
     quizes = [json.loads(QUIZ.parse_obj(x).json()) for x in QUIZ_COL.find().limit(5)]
     return {"quizes": quizes}
 
 @router.get("/{id}") #READONE
 async def listQuiz(id):
-    quiz = QUIZ_COL.find_one({"_id":id}).json()
-    return {"id": id,"quiz":quiz}
+
+    quiz = QUIZ_COL.find_one({"_id":ObjectId(id)})
+    return QUIZ.parse_obj(quiz)
--- app/schema/schema.py
+++ app/schema/schema.py
@@ -1,8 +1,24 @@
 
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator,Field as PydanticField
 
 from typing import Optional,List
 from datetime import datetime
+
+from bson import ObjectId
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
 
 class CHOICE(BaseModel):
     text: str
@@ -15,6 +31,7 @@ class QUESTION(BaseModel):
     multipleanswer: bool = False
 
 class QUIZ(BaseModel):
+    id: PyObjectId = PydanticField(default_factory=PyObjectId, alias="_id")
     author: str
     questions: List[QUESTION]
     created_at: datetime = None #auto update creation time
@@ -26,3 +43,12 @@ class QUIZ(BaseModel):
     @validator('last_modified', pre=True, always=True)
     def default_ts_modified(cls, v, *, values, **kwargs):
         return v or values['created_at']
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True #required for the _id 
+        json_encoders = {ObjectId: str}
+
+class QUIZES(BaseModel):
+    quizes: List[QUIZ]
+
+
